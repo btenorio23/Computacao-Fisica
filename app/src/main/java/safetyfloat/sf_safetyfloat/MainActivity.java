bd3026a0533c89b6f2895c3e599df5bc4dce195d
MarcosFMS <ace6d52f>
2016-11-16 02:03:35 -0200

    Utilizacao dos spinners, menu e grafico.

--- app/src/main/java/safetyfloat/sf_safetyfloat/MainActivity.java
+++ app/src/main/java/safetyfloat/sf_safetyfloat/MainActivity.java
@@ -1,8 +1,12 @@
 package safetyfloat.sf_safetyfloat;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +16,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener {
+
+    RelativeLayout mainContentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +40,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -40,6 +57,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Adiciona novo Layout no main_content
+        mainContentLayout = (RelativeLayout) findViewById(R.id.content_main);
+        loadMonitores();
     }
 
     @Override
@@ -80,18 +101,78 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
+        if (id == R.id.monitores) {
+            loadMonitores();
+        } else if (id == R.id.leituras) {
+            loadLeituras();
+        } else if (id == R.id.alertas) {
+            loadAlertas();
+        } else if (id == R.id.configuracoes) {
+            loadConfiguracoes();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void loadGraph(DataPoint[] points) {
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4.8, 6)
+        });
+        graph.addSeries(series);
+    }
+
+    public void loadSpinner(String[] floats) {
+        floats = new String[]{"boia1", "boia2", "boia3"};
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, floats);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        String boiaSelecionada = (String)parent.getItemAtPosition(pos);
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, boiaSelecionada, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    public void loadContentLayout(int layout, int id){
+        mainContentLayout.removeAllViews();
+        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentLayout = layoutInflater.inflate(layout, (ViewGroup) findViewById(id));
+        mainContentLayout.addView(contentLayout);
+    }
+
+    public void loadMonitores(){
+        loadContentLayout(R.layout.monitores, R.id.monitores);
+        loadGraph(null);//carrega o grafico com um Array de DataPoint
+        loadSpinner(null);//carrega o spinner com um array de String
+    }
+
+    public void loadLeituras(){
+        loadContentLayout(R.layout.leituras, R.id.leituras);
+    }
+
+    public void loadAlertas(){
+        loadContentLayout(R.layout.alertas, R.id.alertas);
+    }
+
+    public void loadConfiguracoes(){
+        loadContentLayout(R.layout.configuracoes, R.id.configuracoes);
     }
 }
