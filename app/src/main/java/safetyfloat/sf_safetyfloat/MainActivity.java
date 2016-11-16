bc341e5eca43f55610ae2976b72b36f17aeb7635
MarcosFMS <ace6d52f>
2016-11-16 18:09:44 -0200

    Início da tela de leituras.

--- app/src/main/java/safetyfloat/sf_safetyfloat/MainActivity.java
+++ app/src/main/java/safetyfloat/sf_safetyfloat/MainActivity.java
@@ -1,6 +1,9 @@
 package safetyfloat.sf_safetyfloat;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -22,8 +25,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -32,6 +42,11 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener {
 
     RelativeLayout mainContentLayout;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +76,9 @@ public class MainActivity extends AppCompatActivity
         //Adiciona novo Layout no main_content
         mainContentLayout = (RelativeLayout) findViewById(R.id.content_main);
         loadMonitores();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -140,7 +158,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        String boiaSelecionada = (String)parent.getItemAtPosition(pos);
+        String boiaSelecionada = (String) parent.getItemAtPosition(pos);
         Context context = getApplicationContext();
         Toast toast = Toast.makeText(context, boiaSelecionada, Toast.LENGTH_SHORT);
         toast.show();
@@ -151,28 +169,166 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    public void loadContentLayout(int layout, int id){
+    public void loadContentLayout(int layout, int id) {
         mainContentLayout.removeAllViews();
-        LayoutInflater layoutInflater = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View contentLayout = layoutInflater.inflate(layout, (ViewGroup) findViewById(id));
         mainContentLayout.addView(contentLayout);
     }
 
-    public void loadMonitores(){
+    public void loadMonitores() {
         loadContentLayout(R.layout.monitores, R.id.monitores);
         loadGraph(null);//carrega o grafico com um Array de DataPoint
         loadSpinner(null);//carrega o spinner com um array de String
     }
 
-    public void loadLeituras(){
+    public void loadLeituras() {
         loadContentLayout(R.layout.leituras, R.id.leituras);
+        String[][] testes = new String[3][3];
+        testes[0] = new String[]{"1", "2", "3"};
+        testes[1] = new String[]{"4", "5", "6"};
+        testes[2] = new String[]{"7", "8", "9"};
+        loadLeiturasTable(testes);
+        //loadTable();
     }
 
-    public void loadAlertas(){
+    public void loadAlertas() {
         loadContentLayout(R.layout.alertas, R.id.alertas);
     }
 
-    public void loadConfiguracoes(){
+    public void loadConfiguracoes() {
         loadContentLayout(R.layout.configuracoes, R.id.configuracoes);
+    }
+
+    public void loadTable(){
+        TableLayout t1;
+
+        TableLayout tl = (TableLayout) findViewById(R.id.tb_leituras);
+        TableRow tr_head = new TableRow(this);
+        tr_head.setBackgroundColor(Color.GRAY);
+        tr_head.setLayoutParams(new TableRow.LayoutParams(
+                TableRow.LayoutParams.FILL_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT));
+        Integer count=0;
+            String date = "data";
+            Double weight_kg = 20.0;
+// Create the table row
+            TableRow tr = new TableRow(this);
+            if(count%2!=0) tr.setBackgroundColor(Color.GRAY);
+            tr.setId(100+count);
+            tr.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.FILL_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+
+//Create two columns to add as table data
+            // Create a TextView to add date
+            TextView labelDATE = new TextView(this);
+            labelDATE.setId(200+count);
+            labelDATE.setText(date);
+            labelDATE.setPadding(2, 0, 5, 0);
+            labelDATE.setTextColor(Color.WHITE);
+            tr.addView(labelDATE);
+            TextView labelWEIGHT = new TextView(this);
+            labelWEIGHT.setId(200+count);
+            labelWEIGHT.setText(weight_kg.toString());
+            labelWEIGHT.setTextColor(Color.WHITE);
+            tr.addView(labelWEIGHT);
+
+// finally add this to the table row
+            tl.addView(tr, new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.FILL_PARENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT));
+            count++;
+    }
+
+    public void loadLeiturasTable(String[][] content) {
+        TableLayout tl = (TableLayout) findViewById(R.id.tb_leituras);
+        //Carrega valores dos resources
+        int padding = (int) getResources().getDimension(R.dimen.tb_leituras_padding);
+        int horarioWidth = (int) getResources().getDimension(R.dimen.tb_header_horario_width);
+        int medicaoWidth = (int) getResources().getDimension(R.dimen.tb_header_medicao_width);
+        int alertaWidth = (int) getResources().getDimension(R.dimen.tb_header_alerta_width);
+        int marginRight = (int) getResources().getDimension(R.dimen.tb_cells_margin_right);
+        int marginBottom = (int) getResources().getDimension(R.dimen.tb_cells_margin_bottom);
+        Drawable backgroudShape = getResources().getDrawable(R.drawable.cell_shape);
+        //Cria layout a ser utilizado pelas celulas da tabela
+        ViewGroup.MarginLayoutParams layout = new ViewGroup.MarginLayoutParams(horarioWidth, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layout.setMargins(0, 0, marginRight, marginBottom);
+
+        for (int i = 0; i < content.length; i++) {
+            TableRow tr = new TableRow(getApplicationContext());
+            tr.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.FILL_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+            //Cria tres colunas (horario, medicao e alerta) e envia para a linha da tabela
+            //Cria coluna horario
+            TextView horario = new TextView(this);
+            horario.setLayoutParams(layout);
+            horario.setText("10");
+            horario.setTextColor(Color.BLACK);
+            horario.setPadding(padding, padding, padding, padding);
+            horario.setBackground(backgroudShape);
+            tr.addView(horario);
+
+            //Cria coluna horario
+            TextView medicao = new TextView(this);
+            medicao.setLayoutParams(layout);
+            medicao.setWidth(medicaoWidth);
+            medicao.setText("20");
+            medicao.setTextColor(Color.BLACK);
+            medicao.setPadding(padding, padding, padding, padding);
+            medicao.setBackground(backgroudShape);
+            tr.addView(medicao);
+
+            //Cria coluna horario
+            TextView alerta = new TextView(this);
+            alerta.setLayoutParams(layout);
+            alerta.setWidth(alertaWidth);
+            alerta.setText("30");
+            alerta.setTextColor(Color.BLACK);
+            alerta.setPadding(padding, padding, padding, padding);
+            alerta.setBackground(backgroudShape);
+            tr.addView(alerta);
+            tl.addView(tr, new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.FILL_PARENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT));
+        }
+
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
